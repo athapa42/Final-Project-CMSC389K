7c2a15ea7c0b5b75e4b55f1149e04ceb4616f207
[Nahom <nahomhaile72@gmail.com>
2019-12-05 06:54:36 -0500

    not finish yet

--- routes/chat.js
+++ routes/chat.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.render('chat');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 module.exports = router;
